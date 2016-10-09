4bb49461c4e7e5aa87b6f79351127aca84ff5b21
Johnzeng <happy_zengda@126.com>
2016-10-09 14:14:31 +0800

    fix file upload bug

--- File.go
+++ File.go
@@ -1,7 +1,10 @@
 package lean
 
 import (
+	"encoding/json"
 	"github.com/parnurzeal/gorequest"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
@@ -14,20 +17,60 @@ type File struct {
 	Bucket string `json:"bucket, omitempt"`
 }
 
-//content-type can be text/plain,image/*, or empty
-func (this *leanClient) UploadFile(fileName, contentType string, file *os.File) *Agent {
+func (this *leanClient) UploadPlainText(fileName, content string) (*File, error) {
 	url := UrlBase + "/files/" + fileName
 	request := gorequest.New()
 	superAgent := request.Post(url).
-		Send(file)
+		Send(content)
 
-	if "" != contentType {
-		superAgent.Set("Content-Type", contentType)
-	}
+	superAgent.Set("Content-Type", "text/plain")
 	agent := &Agent{
 		superAgent: superAgent,
 		client:     this,
 	}
 
-	return agent
+	if err := agent.Do(); nil != err {
+		return nil, err
+	}
+
+	ret := &File{}
+
+	if err := agent.ScanResponse(ret); nil != err {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+//content-type can be text/plain,image/*, or empty
+func (this *leanClient) UploadFile(fileName, contentType string, file *os.File) (*File, error) {
+	//as gorequest doesn't have a good support for binary request, so I have to make the request by native http.request api,this is bad
+	url := UrlBase + "/files/" + fileName
+
+	if "" != contentType {
+		contentType = "application/json"
+	}
+	httpRequest, err := http.NewRequest("POST", url, file)
+	if nil != err {
+		return nil, err
+	}
+	httpRequest.Header.Add("X-LC-Key", this.appKey)
+	httpRequest.Header.Add("X-LC-Id", this.appId)
+	client := http.Client{}
+	httpResponse, err := client.Do(httpRequest)
+
+	if nil != err {
+		return nil, err
+	}
+
+	ret := &File{}
+	retBytes, err := ioutil.ReadAll(httpResponse.Body)
+	if nil != err {
+		return nil, err
+	}
+	if jsonErr := json.Unmarshal(retBytes, ret); nil != jsonErr {
+		return nil, jsonErr
+	}
+
+	return ret, nil
 }
--- File_test.go
+++ File_test.go
@@ -5,7 +5,26 @@ import (
 	"testing"
 )
 
-func TestFileUpload(t *testing.T) {
+func TestUploadPlainText(t *testing.T) {
+	client := NewClient(os.Getenv("LEAN_APPID"),
+		os.Getenv("LEAN_APPKEY"),
+		os.Getenv("LEAN_MASTERKEY"))
+	//	uploafile, openErr := os.Open("uploadtest.txt")
+	//	if nil != openErr {
+	//		t.Error(openErr.Error())
+	//		return
+	//	}
+	ret, err := client.UploadPlainText("test.txt", "this is a test")
+
+	if nil != err {
+		t.Error(err.Error())
+		return
+	}
+
+	t.Logf("%v", ret)
+}
+
+func TestUploadFile(t *testing.T) {
 	client := NewClient(os.Getenv("LEAN_APPID"),
 		os.Getenv("LEAN_APPKEY"),
 		os.Getenv("LEAN_MASTERKEY"))
@@ -14,9 +33,12 @@ func TestFileUpload(t *testing.T) {
 		t.Error(openErr.Error())
 		return
 	}
-	agent := client.UploadFile("test.txt", "text/plain", uploafile)
-	if err := agent.Do(); nil != err {
+	ret, err := client.UploadFile("test2.txt", "image/png", uploafile)
+
+	if nil != err {
 		t.Error(err.Error())
 		return
 	}
+
+	t.Logf("%v", ret)
 }
